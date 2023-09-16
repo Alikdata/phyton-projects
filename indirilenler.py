d7322604382e8d73eaaf905f0a4a2b947ed8d8c5
Alikdata <141666860+Alikdata@users.noreply.github.com>
2023-09-16 22:38:00 +0300

    Add files via upload

---
+++ indirilenler.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import jovian
+
+
+# In[10]:
+
+
+weight=int(input("enter your weight in kq "))
+height=int(input("enter your height in sm "))
+gender= input("enter your gender ")
+age=int(input("enter your age "))
+BMI=(weight*weight)/(height*1.8)               
+print(BMI)
+
+
+# In[11]:
+
+
+if BMI >0:
+    if(BMI<16):
+        print("ali you are very thin")
+    elif(BMI>=16):
+        print("ali you are thin")
+    elif(BMI<25):
+        print("ali you are normal")
+    elif(BMI>=25):
+        print("ali you are overweight")
+    else: 
+        print("enter value")
+
+
+# In[12]:
+
+
+jovian.commit("BMI calculator for phyton practise")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
